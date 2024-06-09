02c8b16a5092f618363df09601322afde50fc0e3
Kirill <kirshuvl@gmail.com>
2024-06-10 03:13:59 +0400

    Update DashboardScreen

--- src/screens/dashboard/Dashboard.Screen.tsx
+++ src/screens/dashboard/Dashboard.Screen.tsx
@@ -1,17 +1,20 @@
 import clsx from 'clsx'
 import type { Component } from 'solid-js'
+import { DashboardProvider } from '../../context/dashboard'
 import styles from './Dashboard.Screen.module.scss'
 
 export const DashboardScreen: Component = () => {
     return (
-        <div class={clsx(styles.dashboard)}>
-            <div class={clsx(styles.column, styles.left)}>
-                <div class={clsx(styles.card)}>CoursesBlock</div>
+        <DashboardProvider>
+            <div class={clsx(styles.dashboard)}>
+                <div class={clsx(styles.column, styles.left)}>
+                    <div class={clsx(styles.card)}>CoursesBlock</div>
+                </div>
+                <div class={clsx(styles.column, styles.right)}>
+                    <div class={clsx(styles.card)}>SeminarsBlock</div>
+                    <div class={clsx(styles.card)}>HomeworksBlock</div>
+                </div>
             </div>
-            <div class={clsx(styles.column, styles.right)}>
-                <div class={clsx(styles.card)}>SeminarsBlock</div>
-                <div class={clsx(styles.card)}>HomeworksBlock</div>
-            </div>
-        </div>
+        </DashboardProvider>
     )
 }
