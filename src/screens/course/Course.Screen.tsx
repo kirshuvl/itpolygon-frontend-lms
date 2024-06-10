8bd5b55b00ccd6400ccfbba8ad86acb56f63e937
Kirill <kirshuvl@gmail.com>
2024-06-10 14:07:25 +0400

    Create CourseScreen

---
+++ src/screens/course/Course.Screen.tsx
@@ -0,0 +1,27 @@
+import clsx from 'clsx'
+import type { Component } from 'solid-js'
+import { CourseProvider } from '../../context/course'
+import styles from './Course.Screen.module.scss'
+
+export const CourseScreen: Component = () => {
+    return (
+        <CourseProvider>
+            <div class={clsx(styles.dashboard)}>
+                <div class={clsx(styles.column, styles.left)}>
+                    <div class={clsx(styles.card)}>Временный блок</div>
+                </div>
+                <div class={clsx(styles.column, styles.right)}>
+                    <div class={clsx(styles.card)}>
+                        <div class={clsx(styles.skeleton)}>Временный блок</div>
+                    </div>
+                    <div class={clsx(styles.card)}>
+                        <div class={clsx(styles.skeleton)}>Временный блок</div>
+                    </div>
+                    <div class={clsx(styles.card)}>
+                        <div class={clsx(styles.skeleton)}>Временный блок</div>
+                    </div>
+                </div>
+            </div>
+        </CourseProvider>
+    )
+}
