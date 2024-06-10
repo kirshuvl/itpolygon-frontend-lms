c00572b2151d64c2c794a6b7ce36810a617b9ef0
Kirill <kirshuvl@gmail.com>
2024-06-10 14:12:13 +0400

    Update CourseScreen

--- src/screens/course/Course.Screen.tsx
+++ src/screens/course/Course.Screen.tsx
@@ -1,6 +1,7 @@
 import clsx from 'clsx'
 import type { Component } from 'solid-js'
 import { CourseProvider } from '../../context/course'
+import { TopicsBlock } from '../../widgets/TopicsBlock/TopicsBlock'
 import styles from './Course.Screen.module.scss'
 
 export const CourseScreen: Component = () => {
@@ -8,7 +9,9 @@ export const CourseScreen: Component = () => {
         <CourseProvider>
             <div class={clsx(styles.dashboard)}>
                 <div class={clsx(styles.column, styles.left)}>
-                    <div class={clsx(styles.card)}>Временный блок</div>
+                    <div class={clsx(styles.card)}>
+                        <TopicsBlock />
+                    </div>
                 </div>
                 <div class={clsx(styles.column, styles.right)}>
                     <div class={clsx(styles.card)}>
