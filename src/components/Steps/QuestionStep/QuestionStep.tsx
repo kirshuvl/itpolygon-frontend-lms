e3cb5aa3add7eef5144ef1ade5a4b4a283142398
Kirill <kirshuvl@gmail.com>
2024-06-12 16:02:18 +0400

    Update QuestionStep & ResourseProvider

--- src/components/Steps/QuestionStep/QuestionStep.tsx
+++ src/components/Steps/QuestionStep/QuestionStep.tsx
@@ -1,25 +1,60 @@
-import { type Component, For } from 'solid-js'
+import { type Component, For, createSignal } from 'solid-js'
 
 import clsx from 'clsx'
 import { Button, InputField } from 'itpolygon-ui-dev'
+import { useFormHandler } from 'solid-form-handler'
+import { yupSchema } from 'solid-form-handler/yup'
+import * as yup from 'yup'
 import { useResourseStateContext } from '../../../context/universal'
 import type { QuestionStepBodyInterface } from '../../../types/steps'
 import { EditorBlock } from '../../Editor/EditorBlock'
 import styles from './QuestionStep.module.scss'
 
-export const QuestionStep: Component = () => {
-    const { currentStep } = useResourseStateContext()
-    const stepBody = currentStep()?.body as QuestionStepBodyInterface
+type UserAnswer = {
+    answer: string
+}
 
+export const userSchema: yup.Schema<UserAnswer> = yup.object({
+    answer: yup.string().required('Введите ответ'),
+})
+
+export const QuestionStep: Component = () => {
+    const formHandler = useFormHandler(yupSchema(userSchema))
+    const { formData } = formHandler
+
+    const {
+        currentStep,
+        actions: { createUserAnswerForQuestionStep },
+    } = useResourseStateContext()
+    const stepBody = () => currentStep()?.body as QuestionStepBodyInterface
+
+    const [isLoading, setIsLoading] = createSignal(false)
+
+    const buttonClick = async () => {
+        setIsLoading(true)
+        await createUserAnswerForQuestionStep({ answer: formData().answer })
+        setIsLoading(false)
+    }
     return (
         <>
             <div class={clsx(styles.body)}>
-                <For each={stepBody.text.blocks}>{(block) => <EditorBlock block={block} />}</For>
+                <For each={stepBody().text.blocks}>{(block) => <EditorBlock block={block} />}</For>
                 <div class={clsx(styles.row)}>
-                    <InputField placeholder="Введите ответ" />
-                    <Button value="Ответить" />
+                    <InputField
+                        type="text"
+                        placeholder="Введите пароль"
+                        name="answer"
+                        formHandler={formHandler}
+                    />
+                    <Button
+                        value="Ответить"
+                        onClick={() => buttonClick()}
+                        disabled={formHandler.isFormInvalid()}
+                        loading={isLoading()}
+                    />
                 </div>
             </div>
+            <div>{JSON.stringify(stepBody().userAnswers)}</div>
         </>
     )
 }
