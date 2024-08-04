89225582729ba91aa8a27f5149f99ddc04efbe10
Kirill <kirshuvl@gmail.com>
2024-08-04 22:12:40 +0400

    Create LessonProgress

---
+++ src/components/LessonProgress/LessonProgress.tsx
@@ -0,0 +1,21 @@
+import { ProgressBarHorizontal } from 'itpolygon-ui-dev'
+import { type Component, createEffect, createSignal } from 'solid-js'
+import { useResourseStateContext } from '../../context/universal'
+
+export const LessonProgress: Component = () => {
+    const { resource } = useResourseStateContext()
+    const [percent, setPercent] = createSignal(0)
+
+    createEffect(() => {
+        const res = resource();
+        if (res?.steps) {
+            setPercent((res.steps.filter((step) => step.userEnroll?.status === 'OK').length / res.steps.length) * 100);
+        }
+    })
+    return (
+        <div>
+            { }
+            <ProgressBarHorizontal percent={percent()} />
+        </div>
+    )
+}
--- src/screens/lesson/Lesson.Screen.tsx
+++ src/screens/lesson/Lesson.Screen.tsx
@@ -2,6 +2,7 @@ import type { Component } from 'solid-js'
 
 import clsx from 'clsx'
 
+import { LessonProgress } from '../../components/LessonProgress/LessonProgress'
 import { ResourseProvider } from '../../context/universal'
 import { CurrentStepBlock } from '../../widgets/CurrentStepBlock/CurrentStepBlock'
 import { StepsBlock } from '../../widgets/StepsBlock/StepsBlock'
@@ -14,6 +15,9 @@ export const LessonScreen: Component = () => {
                 <div class={clsx(styles.dashboard)}>
                     <CurrentStepBlock />
                     <div class={clsx(styles.column, styles.right)}>
+                        <div class={clsx(styles.card, styles.sticky)}>
+                            <LessonProgress />
+                        </div>
                         <div class={clsx(styles.card, styles.sticky)}>
                             <StepsBlock />
                         </div>
